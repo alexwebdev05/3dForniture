618d7afffb1300bac39aa1bb1eaf9738c8b44fde
sniffsec <sniffsec@proton.me>
2024-07-13 11:47:03 +0200

    Production error solved

--- src/fornitures/fornitures.js
+++ src/fornitures/fornitures.js
@@ -2,49 +2,49 @@ export const fornitures = {
     chair1: {
         name: 'Confortable 7-ksj',
         description: 'In today’s fast-paced work environment, comfort and efficiency are paramount. Our Confortable 7-ksj Chair ensures that you can focus on your tasks without the distraction of discomfort. Whether you are working from home or in a corporate office, this chair is designed to enhance your sitting experience and support your wellbeing.',
-        path: 'src/assets/models/chair1.glb',
+        path: 'models/chair1.glb',
         type: 'chair'
     },
     chair2: {
         name: 'Office black chair',
         description: 'In today’s fast-paced world, having reliable and high-quality furnishings is essential. The Office black chair exemplifies top-notch craftsmanship and design, ensuring that your workspace is not only functional but also stylish and comfortable.',
-        path: 'src/assets/models/chair2.glb',
+        path: 'models/chair2.glb',
         type: 'chair'
     },
     dresser1: {
         name: 'Big dresser',
         description: 'Experience the perfect blend of luxury and practicality with the Big dresser. Crafted with the finest materials, this piece guarantees longevity and exceptional performance, making it an indispensable addition to any setting.',
-        path: 'src/assets/models/dresser1.glb',
+        path: 'models/dresser1.glb',
         type: 'dresser'
     },
     dresser2: {
         name: 'Small dresser',
         description: 'The Small dresser represents the pinnacle of ergonomic design, providing unparalleled comfort and support. Its sleek aesthetics and robust construction make it a standout choice for those who value both form and function.',
-        path: 'src/assets/models/dresser2.glb',
+        path: 'models/dresser2.glb',
         type: 'dresser'
     },
     shelf1: {
         name: 'Standard shelf',
         description: 'Elevate your environment with the Standard shelf, a testament to superior quality and innovative design. This piece is engineered to deliver outstanding durability and a sophisticated look that complements any decor.',
-        path: 'src/assets/models/shelf1.glb',
+        path: 'models/shelf1.glb',
         type: 'shelf'
     },
     shelf2: {
         name: 'Esthetic shelf',
         description: 'With the Esthetic shelf, you can enjoy the perfect combination of style, comfort, and reliability. This product is designed to meet the highest standards of quality, ensuring it remains a cherished part of your space for years to come.',
-        path: 'src/assets/models/shelf2.glb',
+        path: 'models/shelf2.glb',
         type: 'shelf'
     },
     light1: {
         name: 'Wall lights',
         description: 'Discover the excellence of the Wall lights, meticulously crafted to provide an optimal user experience. Its premium build and elegant design make it a must-have for those who seek the best in both aesthetics and functionality.',
-        path: 'src/assets/models/light1.glb',
+        path: 'models/light1.glb',
         type: 'light'
     },
     light2: {
         name: 'Roof light',
         description: 'The Roof light sets a new standard in high-quality furnishings. Its exceptional design and robust construction ensure that it not only meets but exceeds your expectations, offering unparalleled comfort and style.',
-        path: 'src/assets/models/light2.glb',
+        path: 'models/light2.glb',
         type: 'light'
     }
 }
